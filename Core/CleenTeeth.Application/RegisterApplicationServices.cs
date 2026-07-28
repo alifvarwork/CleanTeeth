807fe3edd8684a8581d6cb3031574278939acd57
a.fazeli <a.fazeli@irisaco.com>
2026-07-28 15:32:45 +0330

    Added RegisterApplicationServices.

---
+++ Core/CleenTeeth.Application/RegisterApplicationServices.cs
@@ -0,0 +1,20 @@
+﻿using CleenTeeth.Application.Features.DentalOffices.Commands.CreateDentalOffice;
+using CleenTeeth.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail;
+using CleenTeeth.Application.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleenTeeth.Application;
+
+public static class RegisterApplicationServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        // Register application services here
+        services.AddTransient<IMediator, SimpleMediator>();
+
+        services.AddScoped<IRequestHandler<CreateDentalOfficeCommand, Guid>, CreateDentalOfficeCommandHandler>();
+        services.AddScoped<IRequestHandler<GetDentalOfficeDetailQuery, DentalOfficeDetailDTO>, GetDentalOfficeDetailQueryHandler>();
+        
+        return services;
+    }
+}
